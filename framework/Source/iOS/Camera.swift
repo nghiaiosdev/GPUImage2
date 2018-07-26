af0fcf398013a876d998cd6d95b6f3566a0cf79a
Luu Nguyen <luunm@vng.com.vn>
2018-07-26 17:50:04 +0700

    Switch camera

--- framework/Source/iOS/Camera.swift
+++ framework/Source/iOS/Camera.swift
@@ -47,6 +47,28 @@ public class Camera: NSObject, ImageSource, AVCaptureVideoDataOutputSampleBuffer
     public var location:PhysicalCameraLocation {
         didSet {
             // TODO: Swap the camera locations, framebuffers as needed
+            
+            if let device = location.device() {
+                do {
+                    let newInput = try AVCaptureDeviceInput(device: device)
+
+                    captureSession.beginConfiguration()
+
+                    captureSession.removeInput(videoInput)
+
+                    if captureSession.canAddInput(newInput) {
+                        captureSession.addInput(newInput)
+                        videoInput = newInput
+                    } else {
+                        captureSession.addInput(videoInput)
+                    }
+
+                    captureSession.commitConfiguration()
+
+                } catch {
+                    debugPrint("Cannot switch camera")
+                }
+            }
         }
     }
     public var runBenchmark:Bool = false
@@ -69,7 +91,7 @@ public class Camera: NSObject, ImageSource, AVCaptureVideoDataOutputSampleBuffer
     public weak var delegate: CameraDelegate?
     public let captureSession:AVCaptureSession
     public let inputCamera:AVCaptureDevice!
-    public let videoInput:AVCaptureDeviceInput!
+    public var videoInput:AVCaptureDeviceInput!
     public let videoOutput:AVCaptureVideoDataOutput!
     public var microphone:AVCaptureDevice?
     public var audioInput:AVCaptureDeviceInput?
@@ -157,11 +179,9 @@ public class Camera: NSObject, ImageSource, AVCaptureVideoDataOutputSampleBuffer
         captureSession.sessionPreset = sessionPreset
 
         
-        if let connections = videoOutput.connections as? [AVCaptureConnection] {
-            for connection in connections {
-                if(connection.isVideoMirroringSupported) {
-                    connection.isVideoMirrored = (location == .frontFacingMirrored)
-                }
+        for connection in videoOutput.connections {
+            if(connection.isVideoMirroringSupported) {
+                connection.isVideoMirrored = (location == .frontFacingMirrored)
             }
         }
 
@@ -198,8 +218,8 @@ public class Camera: NSObject, ImageSource, AVCaptureVideoDataOutputSampleBuffer
         self.captureSessionRestartAttempts = 0
     }
     
-    public func captureOutput(_ captureOutput:AVCaptureOutput!, didOutputSampleBuffer sampleBuffer:CMSampleBuffer!, from connection:AVCaptureConnection!) {
-        guard (captureOutput != audioOutput) else {
+    public func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        guard (output != audioOutput) else {
             self.processAudioSampleBuffer(sampleBuffer)
             return
         }
@@ -213,64 +233,130 @@ public class Camera: NSObject, ImageSource, AVCaptureVideoDataOutputSampleBuffer
         let bufferHeight = CVPixelBufferGetHeight(cameraFrame)
         let currentTime = CMSampleBufferGetPresentationTimeStamp(sampleBuffer)
         
+        debugPrint("bufferSize(\(bufferWidth),\(bufferHeight))")
+        
         CVPixelBufferLockBaseAddress(cameraFrame, CVPixelBufferLockFlags(rawValue:CVOptionFlags(0)))
         sharedImageProcessingContext.runOperationAsynchronously{
             let cameraFramebuffer:Framebuffer
             
             self.delegate?.didCaptureBuffer(sampleBuffer)
+            
             if self.captureAsYUV {
                 let luminanceFramebuffer:Framebuffer
                 let chrominanceFramebuffer:Framebuffer
+                
                 if sharedImageProcessingContext.supportsTextureCaches() {
                     var luminanceTextureRef:CVOpenGLESTexture? = nil
-                    let _ = CVOpenGLESTextureCacheCreateTextureFromImage(kCFAllocatorDefault, sharedImageProcessingContext.coreVideoTextureCache, cameraFrame, nil, GLenum(GL_TEXTURE_2D), GL_LUMINANCE, GLsizei(bufferWidth), GLsizei(bufferHeight), GLenum(GL_LUMINANCE), GLenum(GL_UNSIGNED_BYTE), 0, &luminanceTextureRef)
+                    let _ = CVOpenGLESTextureCacheCreateTextureFromImage(kCFAllocatorDefault,
+                                                                         sharedImageProcessingContext.coreVideoTextureCache,
+                                                                         cameraFrame,
+                                                                         nil,
+                                                                         GLenum(GL_TEXTURE_2D),
+                                                                         GL_LUMINANCE,
+                                                                         GLsizei(bufferWidth),
+                                                                         GLsizei(bufferHeight),
+                                                                         GLenum(GL_LUMINANCE),
+                                                                         GLenum(GL_UNSIGNED_BYTE),
+                                                                         0,
+                                                                         &luminanceTextureRef)
+                    
                     let luminanceTexture = CVOpenGLESTextureGetName(luminanceTextureRef!)
+                    
                     glActiveTexture(GLenum(GL_TEXTURE4))
                     glBindTexture(GLenum(GL_TEXTURE_2D), luminanceTexture)
                     glTexParameteri(GLenum(GL_TEXTURE_2D), GLenum(GL_TEXTURE_WRAP_S), GL_CLAMP_TO_EDGE)
                     glTexParameteri(GLenum(GL_TEXTURE_2D), GLenum(GL_TEXTURE_WRAP_T), GL_CLAMP_TO_EDGE)
-                    luminanceFramebuffer = try! Framebuffer(context:sharedImageProcessingContext, orientation:self.location.imageOrientation(), size:GLSize(width:GLint(bufferWidth), height:GLint(bufferHeight)), textureOnly:true, overriddenTexture:luminanceTexture)
+
+                    luminanceFramebuffer = try! Framebuffer(context:sharedImageProcessingContext,
+                                                            orientation:self.location.imageOrientation(),
+                                                            size:GLSize(width:GLint(bufferWidth), height:GLint(bufferHeight)),
+                                                            textureOnly:true,
+                                                            overriddenTexture:luminanceTexture)
                     
                     var chrominanceTextureRef:CVOpenGLESTexture? = nil
-                    let _ = CVOpenGLESTextureCacheCreateTextureFromImage(kCFAllocatorDefault, sharedImageProcessingContext.coreVideoTextureCache, cameraFrame, nil, GLenum(GL_TEXTURE_2D), GL_LUMINANCE_ALPHA, GLsizei(bufferWidth / 2), GLsizei(bufferHeight / 2), GLenum(GL_LUMINANCE_ALPHA), GLenum(GL_UNSIGNED_BYTE), 1, &chrominanceTextureRef)
+                    let _ = CVOpenGLESTextureCacheCreateTextureFromImage(kCFAllocatorDefault,
+                                                                         sharedImageProcessingContext.coreVideoTextureCache,
+                                                                         cameraFrame,
+                                                                         nil,
+                                                                         GLenum(GL_TEXTURE_2D),
+                                                                         GL_LUMINANCE_ALPHA,
+                                                                         GLsizei(bufferWidth / 2),
+                                                                         GLsizei(bufferHeight / 2),
+                                                                         GLenum(GL_LUMINANCE_ALPHA),
+                                                                         GLenum(GL_UNSIGNED_BYTE),
+                                                                         1,
+                                                                         &chrominanceTextureRef)
+
                     let chrominanceTexture = CVOpenGLESTextureGetName(chrominanceTextureRef!)
+
                     glActiveTexture(GLenum(GL_TEXTURE5))
                     glBindTexture(GLenum(GL_TEXTURE_2D), chrominanceTexture)
                     glTexParameteri(GLenum(GL_TEXTURE_2D), GLenum(GL_TEXTURE_WRAP_S), GL_CLAMP_TO_EDGE)
                     glTexParameteri(GLenum(GL_TEXTURE_2D), GLenum(GL_TEXTURE_WRAP_T), GL_CLAMP_TO_EDGE)
-                    chrominanceFramebuffer = try! Framebuffer(context:sharedImageProcessingContext, orientation:self.location.imageOrientation(), size:GLSize(width:GLint(bufferWidth / 2), height:GLint(bufferHeight / 2)), textureOnly:true, overriddenTexture:chrominanceTexture)
+
+                    chrominanceFramebuffer = try! Framebuffer(context:sharedImageProcessingContext,
+                                                              orientation:self.location.imageOrientation(),
+                                                              size:GLSize(width:GLint(bufferWidth / 2), height:GLint(bufferHeight / 2)),
+                                                              textureOnly:true,
+                                                              overriddenTexture:chrominanceTexture)
                 } else {
                     glActiveTexture(GLenum(GL_TEXTURE4))
-                    luminanceFramebuffer = sharedImageProcessingContext.framebufferCache.requestFramebufferWithProperties(orientation:self.location.imageOrientation(), size:GLSize(width:GLint(bufferWidth), height:GLint(bufferHeight)), textureOnly:true)
+                    
+                    luminanceFramebuffer = sharedImageProcessingContext.framebufferCache
+                        .requestFramebufferWithProperties(orientation:self.location.imageOrientation(),
+                                                          size:GLSize(width:GLint(bufferWidth), height:GLint(bufferHeight)),
+                                                          textureOnly:true)
                     luminanceFramebuffer.lock()
                     
                     glBindTexture(GLenum(GL_TEXTURE_2D), luminanceFramebuffer.texture)
                     glTexImage2D(GLenum(GL_TEXTURE_2D), 0, GL_LUMINANCE, GLsizei(bufferWidth), GLsizei(bufferHeight), 0, GLenum(GL_LUMINANCE), GLenum(GL_UNSIGNED_BYTE), CVPixelBufferGetBaseAddressOfPlane(cameraFrame, 0))
                     
                     glActiveTexture(GLenum(GL_TEXTURE5))
-                    chrominanceFramebuffer = sharedImageProcessingContext.framebufferCache.requestFramebufferWithProperties(orientation:self.location.imageOrientation(), size:GLSize(width:GLint(bufferWidth / 2), height:GLint(bufferHeight / 2)), textureOnly:true)
+                    
+                    chrominanceFramebuffer = sharedImageProcessingContext.framebufferCache
+                        .requestFramebufferWithProperties(orientation:self.location.imageOrientation(),
+                                                          size:GLSize(width:GLint(bufferWidth / 2), height:GLint(bufferHeight / 2)),
+                                                          textureOnly:true)
+
                     chrominanceFramebuffer.lock()
+
                     glBindTexture(GLenum(GL_TEXTURE_2D), chrominanceFramebuffer.texture)
                     glTexImage2D(GLenum(GL_TEXTURE_2D), 0, GL_LUMINANCE_ALPHA, GLsizei(bufferWidth / 2), GLsizei(bufferHeight / 2), 0, GLenum(GL_LUMINANCE_ALPHA), GLenum(GL_UNSIGNED_BYTE), CVPixelBufferGetBaseAddressOfPlane(cameraFrame, 1))
                 }
                 
-                cameraFramebuffer = sharedImageProcessingContext.framebufferCache.requestFramebufferWithProperties(orientation:.portrait, size:luminanceFramebuffer.sizeForTargetOrientation(.portrait), textureOnly:false)
+                cameraFramebuffer = sharedImageProcessingContext.framebufferCache
+                    .requestFramebufferWithProperties(orientation:.portrait,
+                                                      size:luminanceFramebuffer.sizeForTargetOrientation(.portrait),
+                                                      textureOnly:false)
                 
                 let conversionMatrix:Matrix3x3
+
                 if (self.supportsFullYUVRange) {
                     conversionMatrix = colorConversionMatrix601FullRangeDefault
                 } else {
                     conversionMatrix = colorConversionMatrix601Default
                 }
-                convertYUVToRGB(shader:self.yuvConversionShader!, luminanceFramebuffer:luminanceFramebuffer, chrominanceFramebuffer:chrominanceFramebuffer, resultFramebuffer:cameraFramebuffer, colorConversionMatrix:conversionMatrix)
+                
+                convertYUVToRGB(shader:self.yuvConversionShader!,
+                                luminanceFramebuffer:luminanceFramebuffer,
+                                chrominanceFramebuffer:chrominanceFramebuffer,
+                                resultFramebuffer:cameraFramebuffer,
+                                colorConversionMatrix:conversionMatrix)
+                
             } else {
-                cameraFramebuffer = sharedImageProcessingContext.framebufferCache.requestFramebufferWithProperties(orientation:self.location.imageOrientation(), size:GLSize(width:GLint(bufferWidth), height:GLint(bufferHeight)), textureOnly:true)
+                cameraFramebuffer = sharedImageProcessingContext.framebufferCache
+                    .requestFramebufferWithProperties(orientation:self.location.imageOrientation(),
+                                                      size:GLSize(width:GLint(bufferWidth), height:GLint(bufferHeight)),
+                                                      textureOnly:true)
+                
                 glBindTexture(GLenum(GL_TEXTURE_2D), cameraFramebuffer.texture)
                 glTexImage2D(GLenum(GL_TEXTURE_2D), 0, GL_RGBA, GLsizei(bufferWidth), GLsizei(bufferHeight), 0, GLenum(GL_BGRA), GLenum(GL_UNSIGNED_BYTE), CVPixelBufferGetBaseAddress(cameraFrame))
             }
+            
             CVPixelBufferUnlockBaseAddress(cameraFrame, CVPixelBufferLockFlags(rawValue:CVOptionFlags(0)))
             
             cameraFramebuffer.timingStyle = .videoFrame(timestamp:Timestamp(currentTime))
+
             self.updateTargetsWithFramebuffer(cameraFramebuffer)
             
             if self.runBenchmark {
